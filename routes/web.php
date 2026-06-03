d2048d9d20776664c2de12a929fc6c72cdbfbf71
cholis <anoercholis@asia.ac.id>
2026-06-03 09:22:08 +0700

    mastering CRUD-create

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PendudukController;
+use App\Http\Controllers\SuratController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/penduduk',[PendudukController::class, 'dataPenduduk']);
-Route::get('/surat', [PendudukController::class, 'daftarSurat']);
+// Route::get('/surat', [PendudukController::class, 'daftarSurat']);
+
+Route::resource('surat', SuratController::class);
